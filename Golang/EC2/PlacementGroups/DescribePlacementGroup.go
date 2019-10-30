81b11c541cf3a0888789c4b7b10cc3673e5ae154
Peter Menage <peter.menage@cascadeo.com>
2019-10-30 11:54:58 -0700

    Add placement group create, describe, and delete scripts

---
+++ Golang/EC2/PlacementGroups/DescribePlacementGroup.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+var PNme, awsRegion string
+
+func main() {
+	// User input placement groupname
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter Placement Group name:")
+	PNme, _ = reader.ReadString('\n')
+	// remove new line
+	PNme = strings.Replace(PNme, "\n", "", -1)
+	// Region
+	reader3 := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter the region the instance is located:")
+	awsRegion, _ = reader3.ReadString('\n')
+	// remove new line
+	awsRegion = strings.Replace(awsRegion, "\n", "", -1)
+
+	// session
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(awsRegion)},
+	)
+	svc := ec2.New(sess)
+	input := &ec2.DescribePlacementGroupsInput{
+		GroupNames: []*string{
+			aws.String(PNme),
+		},
+	}
+
+	// Describe placement group name provided by user
+	result, err := svc.DescribePlacementGroups(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result)
+}
