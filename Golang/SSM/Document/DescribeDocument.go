359c40768cec0c785ad478a4ef438ee46406c12a
Peter Menage <peter.menage@cascadeo.com>
2019-11-03 08:40:06 -0800

    Add describe Document

---
+++ Golang/SSM/Document/DescribeDocument.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
+)
+
+var DocName, DocReg string
+
+func main() {
+	// user input Document name
+	DocNreader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter Document name:")
+	DocName, _ = DocNreader.ReadString('\n')
+	// remove new line
+	DocName = strings.Replace(DocName, "\n", "", -1)
+
+	// user input document region
+	DocRegreader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter region the document is located in:")
+	DocReg, _ = DocRegreader.ReadString('\n')
+	// remove new line
+	DocReg = strings.Replace(DocReg, "\n", "", -1)
+	// ssm session
+	// session
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(DocReg)},
+	)
+	svc := ssm.New(sess)
+	// use for document content
+	doc := &ssm.DescribeDocumentInput{
+		Name: &DocName,
+	}
+	// Create Document
+	DocResult, err := svc.DescribeDocument(doc)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(DocResult)
+}
